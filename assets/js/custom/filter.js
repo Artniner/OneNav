8f019a5ff13226b6a9dd3c4da4c2f87224cd5349
Artniner <791208791@qq.com>
2020-05-28 19:09:19 +0800

    v 1.0

---
+++ assets/js/custom/filter.js
@@ -0,0 +1,50 @@
+// 左侧导航栏
+$(".main-menu ul > li").click(main_filter);
+$(".menu-group").click(main_filter);
+
+function main_filter(){
+	// 显示 item
+	if ($(this).attr("class") == "active") return false;
+	$(".active").removeClass("active");
+	$(this).addClass("active");
+	$(".item-cell").hide();
+	$($(this).data("class")).fadeIn();
+    
+    // 修改侧边栏状态
+	$(this).parent().parent().addClass("active");
+
+	// 隐藏搜索
+	$(".web-search").hide();
+
+    // 隐藏 logo 边框
+	$(".logo-collapsed").css({border:"none"});
+
+	// 显示 search bar
+	$("#chat .search-field").fadeIn();
+}
+
+// 主 logo
+$(".search-logo").click(function(){
+	$(".web-search").hide();
+	$(".item-cell").fadeIn();
+});
+
+// 左侧导航栏 logo
+$(".logo-collapsed").click(function(){
+	// 显示搜索
+    if ($(".web-search").attr("style") !== "display: none;") return false;
+    $(".item-cell").hide();
+    $(".web-search").fadeIn();
+
+    // 隐藏 item
+    $(".active").removeClass("active");
+
+    // 显示边框
+    $(this).css({
+    	"border-left":"3px solid var(--main-color)",
+    	"border-right":"3px solid var(--main-color)"
+    });
+
+    // 隐藏 search bar
+    $("#chat .search-field").hide();
+});
